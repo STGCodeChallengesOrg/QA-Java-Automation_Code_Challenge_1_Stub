29ff2ea06e5d55404932c6c6c8e5eb073b23d922
AgentGibbs <christian.gibbs@stgconsulting.com>
2016-09-23 12:23:50 -0600

    Added notes to method call

--- src/main/java/Automation1.java
+++ src/main/java/Automation1.java
@@ -7,6 +7,12 @@ public class Automation1 {
 
     }
 
+    /**
+     * Navigates to a web page and gets the title, and verifies that the title matches what is expected.
+     * @param pageUrl String; The URL of the page that should be opened.
+     * @param expectedTitle String; the expected value of the page title.
+     * @return Boolean; represents whether or not the title matches the expected value
+     */
     public boolean GoToPage(String pageUrl, String expectedTitle){
         //TODO Open browser
         //TODO Navigate to pageUrl
@@ -15,4 +21,5 @@ public class Automation1 {
         //TODO Return result
         return false;
     }
+
 }
